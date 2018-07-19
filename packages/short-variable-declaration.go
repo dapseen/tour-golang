2103bf11a390b0bbc221889327dbf7a4b6b8c5db
ajuwon adedapo <dapseen@gmail.com>
2018-07-19 10:12:23 +0100

    day 02

---
+++ packages/short-variable-declaration.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var k int = 3
+
+func main(){
+    var i, j int = 1,2
+    c, python, java := true,false, "no!"
+    
+    fmt.Println(i,j,k,c,python,java)
+}
---
+++ packages/variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+//package level variable decalaration
+var c, python, java bool
+
+func main(){
+    var i int
+    fmt.Println(i, c, python, java)
+}
